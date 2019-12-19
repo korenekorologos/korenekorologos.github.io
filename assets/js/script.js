fa3f13af29f4558bded4227dbe914f5682539b2e
korenekorologos <55328894+korenekorologos@users.noreply.github.com>
2019-12-18 17:07:21 -0700

    filed added.

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+
+function submitBtn() { 
+    var name = document.querySelector("#name").value  
+    var email = document.querySelector("#email").value
+    var message = document.querySelector("#message").value    
+    console.log(name, email, message)
+    
+} 
+
+
+
+
