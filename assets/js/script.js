ba3290f07ad13b4da2e004c96af3f716d8ca51d5
korene korologos <korenekorologos@gmail.com>
2020-01-28 09:24:16 -0700

    contact section has been updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
 
 
-function submitBtn() { 
-    var name = document.querySelector("#name").value  
-    var email = document.querySelector("#email").value
-    var message = document.querySelector("#message").value    
-    console.log(name, email, message)
+// function submitBtn() { 
+//     var name = document.querySelector("#name").value  
+//     var email = document.querySelector("#email").value
+//     var message = document.querySelector("#message").value    
+//     console.log(name, email, message)
     
-} 
+// } 
+
+
+
+
 
 
 
